661d4ae2a385ce86364cb35c0f71d5b185577c04
Valaeraa <valaeraa@gmail.com>
2021-07-21 20:11:46 +0200

    Added seed data and db context

---
+++ Audiophile.Data/AudiophileContext.cs
@@ -0,0 +1,20 @@
+﻿using Audiophile.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audiophile.Data
+{
+    public class AudiophileContext : DbContext
+    {
+        public AudiophileContext(DbContextOptions<AudiophileContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
