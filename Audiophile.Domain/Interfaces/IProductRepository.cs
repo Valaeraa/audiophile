5cb494a2ba6e74bbacf6580ff9c8078b603072bd
Valaeraa <valaeraa@gmail.com>
2021-07-21 20:47:09 +0200

    Created IProductRepository

---
+++ Audiophile.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Audiophile.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audiophile.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllProductsAsync();
+        Task<IReadOnlyList<ProductCategory>> GetProductCategories(); 
+    }
+}
