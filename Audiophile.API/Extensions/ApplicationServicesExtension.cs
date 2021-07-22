5a3d59ff729670693247bdf90dc212d043423875
Valaeraa <valaeraa@gmail.com>
2021-07-22 11:15:25 +0200

    Extended IServiceCollection for better readability & maintainability for services

---
+++ Audiophile.API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,20 @@
+﻿using Audiophile.Data;
+using Audiophile.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Audiophile.API.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
--- Audiophile.API/Startup.cs
+++ Audiophile.API/Startup.cs
@@ -1,3 +1,4 @@
+using Audiophile.API.Extensions;
 using Audiophile.Data;
 using Audiophile.Domain.Interfaces;
 using Microsoft.AspNetCore.Builder;
@@ -35,12 +36,12 @@ namespace Audiophile.API
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddApplicationServices();
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Audiophile.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Audiophile", Version = "v1" });
             });
-
-            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
