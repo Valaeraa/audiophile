7abba83b2bf1a8f043a73513f7af2f66606b82c0
Valaeraa <valaeraa@gmail.com>
2021-07-23 16:03:53 +0200

    Added remaining properties to the AudiophileContext

--- Audiophile.Data/AudiophileContext.cs
+++ Audiophile.Data/AudiophileContext.cs
@@ -10,5 +10,9 @@ namespace Audiophile.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Include> Includes { get; set; }
+        public DbSet<Gallery> Galleries { get; set; }
+        public DbSet<Other> Others { get; set; }
     }
 }
