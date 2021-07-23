2c1cbee7982bb30d0450d7a3b0c98b2a7816138e
Valaeraa <valaeraa@gmail.com>
2021-07-23 16:05:07 +0200

    Added new migration

--- Audiophile.Data/Migrations/20210723135643_InitialCreate.Designer.cs
+++ Audiophile.Data/Migrations/20210723135643_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Audiophile.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Audiophile.Data.Migrations
 {
     [DbContext(typeof(AudiophileContext))]
-    partial class AudiophileContextModelSnapshot : ModelSnapshot
+    [Migration("20210723135643_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Audiophile.Data/Migrations/20210723135643_InitialCreate.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Audiophile.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Mobile = table.Column<string>(type: "TEXT", nullable: true),
+                    Tablet = table.Column<string>(type: "TEXT", nullable: true),
+                    Desktop = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Galleries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstId = table.Column<int>(type: "INTEGER", nullable: true),
+                    SecondId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ThirdId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galleries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Galleries_Images_FirstId",
+                        column: x => x.FirstId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Galleries_Images_SecondId",
+                        column: x => x.SecondId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Galleries_Images_ThirdId",
+                        column: x => x.ThirdId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Slug = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    New = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Features = table.Column<string>(type: "TEXT", nullable: true),
+                    GalleryId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Galleries_GalleryId",
+                        column: x => x.GalleryId,
+                        principalTable: "Galleries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Products_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Includes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Item = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Includes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Includes_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Others",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Slug = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Others", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Others_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Others_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Galleries_FirstId",
+                table: "Galleries",
+                column: "FirstId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Galleries_SecondId",
+                table: "Galleries",
+                column: "SecondId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Galleries_ThirdId",
+                table: "Galleries",
+                column: "ThirdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Includes_ProductId",
+                table: "Includes",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Others_ImageId",
+                table: "Others",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Others_ProductId",
+                table: "Others",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_GalleryId",
+                table: "Products",
+                column: "GalleryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ImageId",
+                table: "Products",
+                column: "ImageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Includes");
+
+            migrationBuilder.DropTable(
+                name: "Others");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Galleries");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
