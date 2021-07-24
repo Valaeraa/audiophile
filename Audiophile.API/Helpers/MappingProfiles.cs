c77ec6e54d20a0b3d623ebbde7b95e5f1c18bff3
Valaeraa <valaeraa@gmail.com>
2021-07-24 13:53:23 +0200

    Configured AutoMapper profiles

---
+++ Audiophile.API/Helpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using Audiophile.API.Dtos;
+using Audiophile.Domain.Entities;
+using AutoMapper;
+
+namespace Audiophile.API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<Image, ImageDto>();
+            CreateMap<Include, IncludeDto>();
+            CreateMap<Gallery, GalleryDto>();
+            CreateMap<Other, OtherDto>();
+        }
+    }
+}
