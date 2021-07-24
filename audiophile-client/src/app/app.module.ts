e1b4f7b23a0e08da84a05b61b76e5240aad71b1d
Valaeraa <valaeraa@gmail.com>
2021-07-25 00:38:52 +0200

    Created core module and navbar component

--- audiophile-client/src/app/app.module.ts
+++ audiophile-client/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
