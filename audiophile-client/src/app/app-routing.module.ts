030d3e812ed14379931e671bd1ffc12bf1e17f35
Valaeraa <valaeraa@gmail.com>
2021-08-02 15:52:19 +0200

    Added routing to the angular app

--- audiophile-client/src/app/app-routing.module.ts
+++ audiophile-client/src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'headphones',
+    loadChildren: () =>
+      import('./headphones/headphones.module').then(
+        (mod) => mod.HeadphonesModule
+      ),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- audiophile-client/src/app/app.module.ts
+++ audiophile-client/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    CoreModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
