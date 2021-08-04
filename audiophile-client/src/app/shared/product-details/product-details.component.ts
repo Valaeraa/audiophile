b391f056e42b29ee1cca9e1b27e7ac3c3b245565
Valaeraa <valaeraa@gmail.com>
2021-08-05 01:37:20 +0200

    Styled product details component

--- audiophile-client/src/app/shared/product-details/product-details.component.ts
+++ audiophile-client/src/app/shared/product-details/product-details.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeadphonesService } from 'src/app/headphones/headphones.service';
+import { IProduct } from '../models/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
 
-  constructor() { }
+  constructor(
+    private headphonesService: HeadphonesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadProduct();
+    console.log(this.product);
   }
 
+  loadProduct(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    const idValue = +id;
+    this.headphonesService.GetHeadphonesById(idValue).subscribe(
+      (product) => {
+        this.product = product;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
