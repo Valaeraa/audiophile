785231cc51f7ccfba2f4b7dfe31c7465ca3c84b1
Valaeraa <valaeraa@gmail.com>
2021-08-05 01:33:37 +0200

    Added IProduct interface

---
+++ audiophile-client/src/app/shared/models/product.ts
@@ -0,0 +1,49 @@
+export interface IProduct {
+  id: number;
+  slug: string;
+  name: string;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  category: string;
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: [
+    {
+      quantity: number;
+      item: string;
+    }
+  ];
+  gallery: {
+    first: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    second: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    third: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+    others: [
+      {
+        slug: string;
+        name: string;
+        image: {
+          mobile: string;
+          tablet: string;
+          desktop: string;
+        };
+      }
+    ];
+  };
+}
