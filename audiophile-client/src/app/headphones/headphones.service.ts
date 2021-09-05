28f0b2a08057e98f3e387f7f7a1137128f9aeb9d
Valaeraa <valaeraa@gmail.com>
2021-09-05 17:28:32 +0200

    Changed service to use environment variable

--- audiophile-client/src/app/headphones/headphones.service.ts
+++ audiophile-client/src/app/headphones/headphones.service.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeadphonesService {
+  baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   getHeadphones(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(
-      'https://localhost:5001/api/products/headphones'
+      `${this.baseUrl}/products?category=headphones`
     );
   }
 
   GetHeadphonesById(id: number): Observable<IProduct> {
-    return this.http.get<IProduct>('https://localhost:5001/api/products/' + id);
+    return this.http.get<IProduct>(`${this.baseUrl}/products/${id}`);
   }
 }
