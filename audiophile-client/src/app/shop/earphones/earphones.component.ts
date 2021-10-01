0cec4464c3bbaa5e38ba231d417343679fa732bc
Valaeraa <valaeraa@gmail.com>
2021-10-01 15:00:57 +0200

    Implemented earphones component

--- audiophile-client/src/app/shop/earphones/earphones.component.ts
+++ audiophile-client/src/app/shop/earphones/earphones.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-earphones',
   templateUrl: './earphones.component.html',
-  styleUrls: ['./earphones.component.scss']
+  styleUrls: ['./earphones.component.scss'],
 })
 export class EarphonesComponent implements OnInit {
+  products: IProduct[] = [];
 
-  constructor() { }
+  constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getEarphones();
   }
 
+  getEarphones(): void {
+    this.shopService.getProducts('earphones').subscribe(
+      (response) => {
+        this.products = response;
+        this.products.map((p) => {
+          if (p.name === 'YX1 Wireless Earphones') {
+            p.display = {
+              mobile:
+                '/assets/category-earphones/mobile/image-yx1-earphones.jpg',
+              tablet:
+                '/assets/category-earphones/tablet/image-yx1-earphones.jpg',
+              desktop:
+                '/assets/category-earphones/desktop/image-yx1-earphones.jpg',
+            };
+          }
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
